81d30594dbbd2524aa2a30d525c7b0c38316b3f1
sairevanthreddy <sairevanthreddy24@gmail.com>
2023-09-05 11:50:43 +0000

    Drupal version 10.1.2

---
+++ dp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Function to check if a package is installed
+is_installed() {
+    dpkg -l | grep -q $1
+}
+
+# Check if Apache is installed
+if ! is_installed apache2; then
+    echo "Installing Apache..."
+    sudo apt-get update
+    sudo apt-get install apache2
+    systemctl start apache2
+    systemctl enable apache2
+else
+    echo "Apache is already installed."
+fi
+
+# Check if PHP is installed
+if ! is_installed php; then
+    echo "Installing PHP..."
+    sudo apt-get update
+    sudo apt-get install php
+else
+    echo "PHP is already installed."
+fi
+
+# Check if MySQL is installed
+if ! is_installed mysql-server; then
+    echo "Installing MySQL..."
+    sudo apt-get update
+    sudo apt-get install mysql-server
+else
+    echo "MySQL is already installed."
+fi
